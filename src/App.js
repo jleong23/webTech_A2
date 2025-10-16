d8d568a094544769c970a0b3ecb75e86b042dfed
Jason Leong <jleongdh@gmail.com>
2025-10-16 16:34:43 +1030

    Git test: Made changes in file to ensure Git and Bootstrap works

--- src/App.js
+++ src/App.js
@@ -1,66 +1,69 @@
-import './cors-redirect';
-import './App.css';
-import { initStrudel, note, hush, evalScope, getAudioContext, webaudioOutput, registerSynthSounds, initAudioOnFirstClick, transpiler } from "@strudel/web";
+import "./cors-redirect";
+import "./App.css";
+import {
+  initStrudel,
+  note,
+  hush,
+  evalScope,
+  getAudioContext,
+  webaudioOutput,
+  registerSynthSounds,
+  initAudioOnFirstClick,
+  transpiler,
+} from "@strudel/web";
 import { useEffect, useRef } from "react";
-import { StrudelMirror } from '@strudel/codemirror';
-import { registerSoundfonts } from '@strudel/soundfonts';
-import { stranger_tune } from './tunes';
+import { StrudelMirror } from "@strudel/codemirror";
+import { registerSoundfonts } from "@strudel/soundfonts";
+import { stranger_tune } from "./tunes";
 
 let globalEditor = null;
 
-
-
 export function SetupButtons() {
-
-  document.getElementById('play').addEventListener('click', () => globalEditor.evaluate());
-  document.getElementById('stop').addEventListener('click', () => globalEditor.stop());
-  document.getElementById('process').addEventListener('click', () => {
-    Proc()
-  }
-  )
-  document.getElementById('process_play').addEventListener('click', () => {
+  document
+    .getElementById("play")
+    .addEventListener("click", () => globalEditor.evaluate());
+  document
+    .getElementById("stop")
+    .addEventListener("click", () => globalEditor.stop());
+  document.getElementById("process").addEventListener("click", () => {
+    Proc();
+  });
+  document.getElementById("process_play").addEventListener("click", () => {
     if (globalEditor != null) {
-      Proc()
-      globalEditor.evaluate()
+      Proc();
+      globalEditor.evaluate();
     }
-  }
-  )
+  });
 }
-
-
 
 export function ProcAndPlay() {
   if (globalEditor != null && globalEditor.repl.state.started == true) {
-    console.log(globalEditor)
-    Proc()
+    console.log(globalEditor);
+    Proc();
     globalEditor.evaluate();
   }
 }
 
 export function Proc() {
-
-  let proc_text = document.getElementById('proc').value
-  let proc_text_replaced = proc_text.replaceAll('<p1_Radio>', ProcessText);
+  let proc_text = document.getElementById("proc").value;
+  let proc_text_replaced = proc_text.replaceAll("<p1_Radio>", ProcessText);
   ProcessText(proc_text);
-  globalEditor.setCode(proc_text_replaced)
+  globalEditor.setCode(proc_text_replaced);
 }
 
 export function ProcessText(match, ...args) {
-
-  let replace = ""
-  if (document.getElementById('flexRadioDefault2').checked) {
-    replace = "_"
+  let replace = "";
+  if (document.getElementById("flexRadioDefault2").checked) {
+    replace = "_";
   }
 
-  return replace
+  return replace;
 }
 
 export default function StrudelDemo() {
-
   const hasRun = useRef(false);
 
   useEffect(() => {
-
     if (!hasRun.current) {
       hasRun.current = true;
       (async () => {
@@ -70,63 +73,95 @@ export default function StrudelDemo() {
           defaultOutput: webaudioOutput,
           getTime: () => getAudioContext().currentTime,
           transpiler,
-          root: document.getElementById('editor'),
+          root: document.getElementById("editor"),
           prebake: async () => {
             initAudioOnFirstClick(); // needed to make the browser happy (don't await this here..)
             const loadModules = evalScope(
-              import('@strudel/core'),
-              import('@strudel/draw'),
-              import('@strudel/mini'),
-              import('@strudel/tonal'),
-              import('@strudel/webaudio'),
+              import("@strudel/core"),
+              import("@strudel/draw"),
+              import("@strudel/mini"),
+              import("@strudel/tonal"),
+              import("@strudel/webaudio")
             );
-            await Promise.all([loadModules, registerSynthSounds(), registerSoundfonts()]);
+            await Promise.all([
+              loadModules,
+              registerSynthSounds(),
+              registerSoundfonts(),
+            ]);
           },
         });
-        Proc()
+        Proc();
       })();
-      document.getElementById('proc').value = stranger_tune
-      SetupButtons()
+      document.getElementById("proc").value = stranger_tune;
+      SetupButtons();
     }
-
   }, []);
-
 
   return (
     <div>
-      <h2>Strudel Demo</h2>
+      <h2 className="bg-light text-center">Strudel Demo</h2>
       <main>
-
         <div className="container-fluid">
           <div className="row">
-            <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
-              <label htmlFor="exampleFormControlTextarea1" className="form-label">Text to preprocess:</label>
-              <textarea className="form-control" rows="15" id="proc" ></textarea>
+            <div
+              className="col-md-8"
+              style={{ maxHeight: "50vh", overflowY: "auto" }}
+            >
+              <label
+                htmlFor="exampleFormControlTextarea1"
+                className="form-label"
+              >
+                Text to preprocess:
+              </label>
+              <textarea className="form-control" rows="15" id="proc"></textarea>
             </div>
             <div className="col-md-4">
-
               <nav>
-                <button id="process" className="btn btn-outline-primary">Preprocess</button>
-                <button id="process_play" className="btn btn-outline-primary">Proc & Play</button>
+                <button id="process" className="btn btn-outline-primary">
+                  Preprocess
+                </button>
+                <button id="process_play" className="btn btn-outline-primary">
+                  Proc & Play
+                </button>
                 <br />
-                <button id="play" className="btn btn-outline-primary">Play</button>
-                <button id="stop" className="btn btn-outline-primary">Stop</button>
+                <button id="play" className="btn btn-outline-primary">
+                  Play
+                </button>
+                <button id="stop" className="btn btn-outline-primary">
+                  Stop
+                </button>
               </nav>
             </div>
           </div>
           <div className="row">
-            <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+            <div
+              className="col-md-8"
+              style={{ maxHeight: "50vh", overflowY: "auto" }}
+            >
               <div id="editor" />
             </div>
             <div className="col-md-4">
               <div className="form-check">
-                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onChange={ProcAndPlay} defaultChecked />
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="flexRadioDefault"
+                  id="flexRadioDefault1"
+                  onChange={ProcAndPlay}
+                  defaultChecked
+                />
                 <label className="form-check-label" htmlFor="flexRadioDefault1">
                   p1: ON
                 </label>
               </div>
               <div className="form-check">
-                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onChange={ProcAndPlay} />
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="flexRadioDefault"
+                  id="flexRadioDefault2"
+                  onChange={ProcAndPlay}
+                />
                 <label className="form-check-label" htmlFor="flexRadioDefault2">
                   p1: HUSH
                 </label>
@@ -134,11 +169,7 @@ export default function StrudelDemo() {
             </div>
           </div>
         </div>
-
-      </main >
-    </div >
+      </main>
+    </div>
   );
-
-
 }
-
