3cfb143b6e1eab123b26ac39db4e3dc800b6e8af
Jason Leong <jleongdh@gmail.com>
2025-11-02 22:48:42 +1030

    Update preProcessTextArea component

--- src/components/PreProessTextArea.jsx
+++ src/components/PreProessTextArea.jsx
@@ -9,9 +9,10 @@ export default function PreProessTextArea({
   outputRootRef,
 }) {
   return (
-    <div className="grid grid-cols-2 gap-2">
-      <div>
-        <label className="block font-semibold text-sm mb-1">
+    <div className="grid md:grid-cols-2 gap-4 mt-4">
+      {/* Text Area Input */}
+      <div className="flex flex-col">
+        <label className="block font-semibold text-red-500 mb-2">
           Text to preprocess:
         </label>
         <textarea
@@ -19,14 +20,15 @@ export default function PreProessTextArea({
           value={procValue}
           onChange={(e) => onProcChange(e.target.value)}
           rows="15"
-          className="w-full border rounded-xl p-2"
+          className="w-full bg-gray-800 text-white border border-gray-700 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-red-600 resize-none h-96"
         />
       </div>
 
-      <div>
+      {/* Editor output */}
+      <div className="flex flex-col gap-3 mt-8">
         <div
           ref={editorRootRef}
-          className="border rounded-xl mt-4 overflow-y-auto h-96"
+          className="bg-gray-800 border border-gray-700 rounded-xl overflow-y-auto h-96 p-2"
         />
         <div ref={outputRootRef} />
       </div>
