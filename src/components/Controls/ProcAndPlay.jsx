fcdbe8864589c2bff8d5e3d7210256c6afcf58f9
Jason Leong <jleongdh@gmail.com>
2025-11-02 22:33:51 +1030

    Update proc and play component

--- src/components/Controls/ProcAndPlay.jsx
+++ src/components/Controls/ProcAndPlay.jsx
@@ -1,10 +1,10 @@
 export default function ProcAndPlay({ onProcPlay }) {
   return (
-    <div className="flex gap-3">
+    <div className="flex justify-center">
       {/* Proc & Play Button */}
       <button
         onClick={onProcPlay}
-        className="flex items-center justify-center px-4 py-2 rounded-md bg-green-700 hover:bg-green-800 text-white font-medium shadow-md transition-colors duration-200"
+        className="px-6 py-3 rounded-lg bg-red-600 hover:bg-red-700 shadow-lg font-bold transition-colors duration-200"
       >
         Proc & Play
       </button>
