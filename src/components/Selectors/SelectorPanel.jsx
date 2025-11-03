eba46576ed10d85c7d52e837dbb8c168cb66547d
Jason Leong <jleongdh@gmail.com>
2025-11-03 11:18:54 +1030

    Hook drumSoundSelector into SelectorPanel component

--- src/components/Selectors/SelectorPanel.jsx
+++ src/components/Selectors/SelectorPanel.jsx
@@ -1,4 +1,8 @@
 import DrumSoundSelector from "./DrumSoundSelector";
-export default function SelectorPanel() {
-  return <DrumSoundSelector />;
+export default function SelectorPanel({ drumBank, setDrumBank }) {
+  return (
+    <div className="mt-4 p-4 bg-gray-800 rounded">
+      <DrumSoundSelector drumBank={drumBank} setDrumBank={setDrumBank} />
+    </div>
+  );
 }
