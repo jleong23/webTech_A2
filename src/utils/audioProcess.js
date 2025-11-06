8c5ffa7127d92696b4a9579c34b42825b6727145
Jason Leong <jleongdh@gmail.com>
2025-11-06 11:05:15 +1030

    Added replacement tokens for other instruments to mute

--- src/utils/audioProcess.js
+++ src/utils/audioProcess.js
@@ -1,13 +1,22 @@
 /**
  *  useStrudelEditor: A util that takes raw Strudel Code (as text) & replaces certain placeholders with actual values depends on user option
  */
-export function audioProcess(text, options = { p1Hush: false, reverb: 0.6 }) {
+export function audioProcess(
+  text,
+  options = { p1Hush: false, p2Hush: false, p3Hush: false, reverb: 0.6 }
+) {
   if (!text) return ""; // returns empty string if no input
   let result = text;
 
   // replace token <p1_Radio> with _ if true or empty string depending on hush
-  const replace = options.p1Hush ? "_" : ""; // use option for future extendsion with other features
-  result = result.replaceAll("<p1_Radio>", replace);
+  const muteDrums = options.p1Hush ? "_" : ""; // use option for future extendsion with other features
+  result = result.replaceAll("<p1_Radio>", muteDrums);
+
+  const muteBass = options.p2Hush ? "_" : ""; // use option for future extendsion with other features
+  result = result.replaceAll("<p2_Radio>", muteBass);
+
+  const muteArp = options.p3Hush ? "_" : ""; // use option for future extendsion with other features
+  result = result.replaceAll("<p3_Radio>", muteArp);
 
   // reverb handler
   result = result.replaceAll("<reverb>", options.reverb);
