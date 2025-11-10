54fc3213cd00dccfd4837292e5fa8ed35a336d19
Jason Leong <jleongdh@gmail.com>
2025-11-10 20:37:55 +1030

    Update button UI

--- src/components/SaveChanges/SaveJSON.jsx
+++ src/components/SaveChanges/SaveJSON.jsx
@@ -1,5 +1,5 @@
 import { FaSave } from "react-icons/fa";
-import { CiSaveUp2 } from "react-icons/ci";
+import { CiSaveDown2 } from "react-icons/ci";
 export default function SaveJSON({ saveToJson, loadFromJson }) {
   return (
     <div className="flex gap-3">
@@ -7,11 +7,18 @@ export default function SaveJSON({ saveToJson, loadFromJson }) {
         onClick={saveToJson}
         className="bg-red-600 hover:bg-red-500 text-white px-4 py-2 rounded-lg shadow-sm transition-colors duration-150"
       >
-        <FaSave size={25} />
+        <span className="flex gap-2">
+          Save
+          <FaSave size={25} />
+        </span>
       </button>
 
       <label className="bg-red-600 hover:bg-red-500 text-white px-4 py-2 rounded-lg shadow-sm cursor-pointer transition-colors duration-150">
-        <CiSaveUp2 size={25} />
+        <span className="flex gap-2">
+          Load
+          <CiSaveDown2 size={25} />
+        </span>
+
         <input
           type="file"
           accept="application/json"
